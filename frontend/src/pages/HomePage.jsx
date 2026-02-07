2ad353539dec71634846bafd0f2a83ca929fa869
ace <aisybatrisyia.aw@gmail.com>
2026-02-07 14:42:24 +0800

    Fix Navbar import casing

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI"
 import api from "../lib/axios";
 import toast from "react-hot-toast";
