690181c8932c6e54dad8b704db087b3c25b7a1b5
ace <aisybatrisyia.aw@gmail.com>
2026-02-07 15:44:37 +0800

    minor changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.inv.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
     baseURL: BASE_URL,
 });
