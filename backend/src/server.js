4f4e055a2264217471ff8191f766d28c07c519a5
ace <aisybatrisyia.aw@gmail.com>
2026-02-07 14:31:07 +0800

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,23 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(express.json());  // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
@@ -27,6 +32,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.inv.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL,
 });
 
 export default api
